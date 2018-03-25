6a5b10fd7048278aa2de79a546fb66e2e5e929e8
Fredrik Thernelius <fredrik.thernelius@gmail.com>
2018-03-25 21:30:16 +0200

    Refactored handler and broke out code to seperate classes

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -18,62 +18,65 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
     private static final String ACCESS_TOKEN = "899157129722048512-YHceWk6N45aBm4g4rSrQH3EXxwPjaPG";
     private static final String ACCESS_TOKEN_SECRET = "hNI81vC3XbSp3tpiQ4m1dywL1KbJ0W3O0LqfcDwCsMAqO";
     private static final int TWEETS_PER_QUERY = 100;
-    private static final int MAX_QUERIES = 5;
 
-
-    private static Map<String, WordItem> resultMap = new HashMap<>();
-
+    //Map<String, WordItem> resultMap = new HashMap<>();
+    TwitterService twitterService = new TwitterService();
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         LOG.info("received: " + input);
-        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
 
         final String hashTag;
         try {
-            hashTag = "#" + readTagFromQueryString(input);
+            hashTag = createHashtagFromQueryString(input);
 
+            Twitter twitter = createTwitterInstance();
+            TwitterUtil.checkLimits(twitter);
+            Query queryMax = createQuery(hashTag);
+            Map<String, WordItem> resultMap = twitterService.getTweets(queryMax, twitter);
+            List<WordItem> wordItems = createSortedList(resultMap);
 
-            ConfigurationBuilder cb = new ConfigurationBuilder();
-            cb.setDebugEnabled(true)
-                    .setOAuthConsumerKey(CONSUMER_KEY)
-                    .setOAuthConsumerSecret(CONSUMER_SECRET)
-                    .setOAuthAccessToken(ACCESS_TOKEN)
-                    .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
-            TwitterFactory tf = new TwitterFactory(cb.build());
-            Twitter twitter = tf.getInstance();
-
-            checkLimits(twitter);
-
-
-            Query queryMax = new Query(hashTag);
-            queryMax.setCount(TWEETS_PER_QUERY);
-            getTweets(queryMax, twitter);
-
-
-            List<WordItem> wordItems = resultMap.values().stream()
-                    .sorted()
-                    .limit(100)
-                    .collect(Collectors.toList());
-
-            wordItems.stream().forEach(LOG::info);
-
-
-            return ApiGatewayResponse.builder()
-                    .setStatusCode(200)
-                    .setObjectBody(wordItems)
-                    .build();
+            return getApiGatewayResponse(wordItems);
 
         } catch (IllegalArgumentException e) {
-            return getApiGatewayResponse(e, 422);
+            return getApiGatewayExceptionResponse(e, 422);  //not correct input
         } catch (IllegalStateException e) {
-            return getApiGatewayResponse(e, 511);
+            return getApiGatewayExceptionResponse(e, 511);  //read limit reached
         } catch (TwitterException e) {
-            return getApiGatewayResponse(e, 500);
+            return getApiGatewayExceptionResponse(e, 500);  //any Twitter exception
         }
     }
 
-    private ApiGatewayResponse getApiGatewayResponse(Exception e, int responseCode) {
+
+
+    private Query createQuery(String hashTag) {
+        Query queryMax = new Query(hashTag);
+        queryMax.setCount(TWEETS_PER_QUERY);
+        return queryMax;
+    }
+
+
+    private Twitter createTwitterInstance() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(CONSUMER_KEY)
+                .setOAuthConsumerSecret(CONSUMER_SECRET)
+                .setOAuthAccessToken(ACCESS_TOKEN)
+                .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        return tf.getInstance();
+    }
+
+
+    private ApiGatewayResponse getApiGatewayResponse(List<WordItem> wordItems) {
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(wordItems)
+                .build();
+    }
+
+
+    private ApiGatewayResponse getApiGatewayExceptionResponse(Exception e, int responseCode) {
         LOG.error(e.getMessage());
 
         return ApiGatewayResponse.builder()
@@ -83,108 +86,114 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
     }
 
 
-    private String readTagFromQueryString(Map<String, Object> input) throws IllegalArgumentException {
+    private String createHashtagFromQueryString(Map<String, Object> input) throws IllegalArgumentException {
         String tag;
         HashMap queryStringParameters = (HashMap) input.get("queryStringParameters");
 
         if (queryStringParameters != null) {
             tag = (String) queryStringParameters.get("twitterTag");
-            if (tag != null && tag.length() > 0) {
-                return tag;
+            if (tag != null && tag.length() > 0 && !tag.startsWith("#")) {
+                return "#" + tag;
             }
         }
 
-        throw new IllegalArgumentException("You must have the query string 'twitterTag=tagname' set in the url");
+        throw new IllegalArgumentException("You must have the query string 'twitterTag=tagname' set in the url, i.e. don't use '#' in the query");
     }
 
 
-    private void checkLimits(Twitter twitter) {
-        Map<String, RateLimitStatus> rateLimitStatus = null;
-        try {
-            rateLimitStatus = twitter.getRateLimitStatus("search");
-            RateLimitStatus searchTweetsRateLimit = rateLimitStatus.get("/search/tweets");
+    private List<WordItem> createSortedList(Map<String, WordItem> resultMap) {
+        List<WordItem> wordItems = resultMap.values().stream()
+                .sorted()
+                .limit(100)
+                .collect(Collectors.toList());
 
-            String message = String.format("You have %d calls remaining out of %d, Limit resets in %d seconds\n",
-                    searchTweetsRateLimit.getRemaining(),
-                    searchTweetsRateLimit.getLimit(),
-                    searchTweetsRateLimit.getSecondsUntilReset());
-
-            LOG.info(message);
-
-            if (searchTweetsRateLimit.getRemaining() == 0) {
-                throw new IllegalStateException(message);
-            }
-
-        } catch (TwitterException e) {
-            e.printStackTrace();
-        }
-
+        wordItems.stream().forEach(LOG::info);
+        return wordItems;
     }
 
-    private void getTweets(Query query, Twitter twitter) throws TwitterException {
-        long maxID = -1;
-        int totalTweets = 0;
+//    private void checkLimits(Twitter twitter) throws TwitterException {
+//        Map<String, RateLimitStatus> rateLimitStatus;
+//
+//        rateLimitStatus = twitter.getRateLimitStatus("search");
+//        RateLimitStatus searchTweetsRateLimit = rateLimitStatus.get("/search/tweets");
+//
+//        String message = String.format("You have %d calls remaining out of %d, Limit resets in %d seconds\n",
+//                searchTweetsRateLimit.getRemaining(),
+//                searchTweetsRateLimit.getLimit(),
+//                searchTweetsRateLimit.getSecondsUntilReset());
+//
+//        LOG.info(message);
+//
+//        if (searchTweetsRateLimit.getRemaining() == 0) {
+//            throw new IllegalStateException(message);
+//        }
+//
+//    }
 
-        //	This is the loop that retrieve multiple blocks of tweets from Twitter
-        for (int queryNumber = 0; queryNumber < MAX_QUERIES; queryNumber++) {
-
-            //does not handle rate limits
-
-            //	If maxID is -1, then this is our first call and we do not want to tell Twitter what the maximum
-            //	tweet id is we want to retrieve.  But if it is not -1, then it represents the lowest tweet ID
-            //	we've seen, so we want to start at it-1 (if we start at maxID, we would see the lowest tweet
-            //	a second time...
-            if (maxID != -1) {
-                query.setMaxId(maxID - 1);
-            }
-
-            //	This actually does the search on Twitter and makes the call across the network
-            QueryResult r = twitter.search(query);
-
-            //	If there are NO tweets in the result set, it is Twitter's way of telling us that there are no
-            //	more tweets to be retrieved.  Remember that Twitter's search index only contains about a week's
-            //	worth of tweets, and uncommon search terms can run out of week before they run out of tweets
-            if (r.getTweets().size() == 0) {
-                break;            // Nothing? We must be done
-            }
-
-
-            //	loop through all the tweets and process them.  In this sample program, we just print them
-            //	out, but in a real application you might save them to a database, a CSV file, do some
-            //	analysis on them, whatever...
-            for (Status s : r.getTweets())                // Loop through all the tweets...
-            {
-                //	Increment our TWEETS_PER_QUERY of tweets retrieved
-                totalTweets++;
-
-                //	Keep track of the lowest tweet ID.  If you do not do this, you cannot retrieve multiple
-                //	blocks of tweets...
-                if (maxID == -1 || s.getId() < maxID) {
-                    maxID = s.getId();
-                }
-
-                handleTweetText(s);
-            }
-        }
-
-
-        LOG.info(String.format("\n\nA total of %d tweets retrieved\n", totalTweets));
-    }
-
-    private void handleTweetText(Status status) {
-        String text = status.getText();
-        List<String> ord = Arrays.asList(text.split("\\s+"));
-        ord.stream().forEach(word -> addWordToMap(word));
-    }
-
-
-    private void addWordToMap(String word) {
-        if (resultMap.containsKey(word)) {
-            WordItem item = resultMap.get(word);
-            resultMap.replace(word, new WordItem(word, item.getCount() + 1));
-        } else {
-            resultMap.put(word, new WordItem(word, 1));
-        }
-    }
+//    private void getTweets(Query query, Twitter twitter) throws TwitterException {
+//        long maxID = -1;
+//        int totalTweets = 0;
+//
+//        //	This is the loop that retrieve multiple blocks of tweets from Twitter
+//        for (int queryNumber = 0; queryNumber < MAX_QUERIES; queryNumber++) {
+//
+//            //does not handle rate limits
+//
+//            //	If maxID is -1, then this is our first call and we do not want to tell Twitter what the maximum
+//            //	tweet id is we want to retrieve.  But if it is not -1, then it represents the lowest tweet ID
+//            //	we've seen, so we want to start at it-1 (if we start at maxID, we would see the lowest tweet
+//            //	a second time...
+//            if (maxID != -1) {
+//                query.setMaxId(maxID - 1);
+//            }
+//
+//            //	This actually does the search on Twitter and makes the call across the network
+//            QueryResult r = twitter.search(query);
+//
+//            //	If there are NO tweets in the result set, it is Twitter's way of telling us that there are no
+//            //	more tweets to be retrieved.  Remember that Twitter's search index only contains about a week's
+//            //	worth of tweets, and uncommon search terms can run out of week before they run out of tweets
+//            if (r.getTweets().size() == 0) {
+//                break;            // Nothing? We must be done
+//            }
+//
+//
+//            //	loop through all the tweets and process them.  In this sample program, we just print them
+//            //	out, but in a real application you might save them to a database, a CSV file, do some
+//            //	analysis on them, whatever...
+//            for (Status s : r.getTweets())                // Loop through all the tweets...
+//            {
+//                //	Increment our TWEETS_PER_QUERY of tweets retrieved
+//                totalTweets++;
+//
+//                //	Keep track of the lowest tweet ID.  If you do not do this, you cannot retrieve multiple
+//                //	blocks of tweets...
+//                if (maxID == -1 || s.getId() < maxID) {
+//                    maxID = s.getId();
+//                }
+//
+//                handleTweetText(s);
+//            }
+//        }
+//
+//
+//        LOG.info(String.format("\n\nA total of %d tweets retrieved\n", totalTweets));
+//    }
+//
+//    private void handleTweetText(Status status) {
+//        String text = status.getText();
+//        List<String> ord = Arrays.asList(text.split("\\s+"));
+//        ord.stream().forEach(word -> addWordToMap(word));
+//    }
+//
+//
+//    private void addWordToMap(String word) {
+//        if (resultMap.containsKey(word)) {
+//            WordItem item = resultMap.get(word);
+//            resultMap.replace(word, new WordItem(word, item.getCount() + 1));
+//        } else {
+//            resultMap.put(word, new WordItem(word, 1));
+//        }
+//    }
 
 }
